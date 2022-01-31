703b564e46e3e8d6ac20b1fe8d0bcc4daeb53b69
Chris-dagostino <dagostino.christian94@gmail.com>
2022-01-31 17:38:19 +0100

    inizio javascript

---
+++ main.js
@@ -0,0 +1,51 @@
+let contElement = document.getElementById('container');
+
+for ( let i = 0 ; i < 100 ; i++ ) {
+
+    const square = 
+}
+
+
+
+
+
+
+const playButton = document.getElementById('play');
+
+playButton.addEventListener('click'
+    function () {
+
+
+        const difficulty = document.getElementById('difficulty').value;
+
+
+
+
+        if (difficulty == "easy") {
+            
+        } else if (difficulty == "medium"){
+            
+        }else (difficulty == "hard"){
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
+
+
